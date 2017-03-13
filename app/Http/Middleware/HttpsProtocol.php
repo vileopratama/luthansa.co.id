b4a04b17a41e65de7160d4cc529914bf38d355ef
suhe <hendarsyahss@gmail.com>
2017-03-13 08:38:50 +0800

    Update Protocol

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Middleware;
+use Closure;
+
+class HttpsProtocol {
+    public function handle($request, Closure $next) {
+            if (!$request->secure()) {
+                return redirect()->secure($request->getRequestUri());
+            }
+            return $next($request); 
+    }
+}
